e97620b2ed84ca16ac1428bc939701254345dc29
Miguel Hernández Gaitan <60478285+MiguelHG2351@users.noreply.github.com>
2020-04-18 01:38:29 -0600

    Updates

---
+++ precache-manifest.38d6f078e1ddfece7d75fff30a7f0399.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e7152e08b0927f4f430cdb63a1d25e0d",
+    "url": "/react-app/index.html"
+  },
+  {
+    "revision": "db4a7462fe70c1459867",
+    "url": "/react-app/static/css/main.df965e27.chunk.css"
+  },
+  {
+    "revision": "2aaa17e5126575d38e9d",
+    "url": "/react-app/static/js/2.b4bc5268.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/react-app/static/js/2.b4bc5268.chunk.js.LICENSE"
+  },
+  {
+    "revision": "db4a7462fe70c1459867",
+    "url": "/react-app/static/js/main.f09f86e6.chunk.js"
+  },
+  {
+    "revision": "9ec6d27828046597bfbc",
+    "url": "/react-app/static/js/runtime-main.61f81c8c.js"
+  },
+  {
+    "revision": "a9a7e7240d871501670352c690d525a1",
+    "url": "/react-app/static/media/admitido.a9a7e724.png"
+  },
+  {
+    "revision": "b07a0a946a24eb547105137502b22bfc",
+    "url": "/react-app/static/media/back.b07a0a94.jpg"
+  },
+  {
+    "revision": "e62c3105691e498966e5cdaf6f5fa5d1",
+    "url": "/react-app/static/media/backdeg.e62c3105.png"
+  },
+  {
+    "revision": "35fdaa3aff4c306f0661fe9c4115fe3f",
+    "url": "/react-app/static/media/err.35fdaa3a.PNG"
+  },
+  {
+    "revision": "9751cf72ba7f033bc3a5947561e96806",
+    "url": "/react-app/static/media/helloWorld.9751cf72.png"
+  },
+  {
+    "revision": "ff3bc29505e818ac4afb1fbad5ee25f2",
+    "url": "/react-app/static/media/josueConTom.ff3bc295.png"
+  },
+  {
+    "revision": "951c2859d33608ef454e3bcb3e0d50b2",
+    "url": "/react-app/static/media/loader.951c2859.gif"
+  },
+  {
+    "revision": "dd083cdadbcf9572ce2f44e070d0ab84",
+    "url": "/react-app/static/media/numeropi.dd083cda.PNG"
+  },
+  {
+    "revision": "9e2f4077600d9d0b55117bf8bde3b040",
+    "url": "/react-app/static/media/pc.9e2f4077.png"
+  },
+  {
+    "revision": "b78fd75e54a351d4c7d591214e2cffd0",
+    "url": "/react-app/static/media/persona.b78fd75e.png"
+  }
+]);
